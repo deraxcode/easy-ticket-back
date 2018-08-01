97e7344e88f36b2c4c7bc7c3ac5d99b7a325dd0b
jorge flores <jorge.flores.s@taisachile.cl>
2018-08-01 15:34:34 -0400

    validators region y comunas

--- routes/region.js
+++ routes/region.js
@@ -1,23 +1,70 @@
 var express = require('express');
 var router = express.Router();
+const { check, validationResult } = require('express-validator/check');
 const Region = require('../model/Region');
+const Response = require('../domain/response');
 
-router.get("/:regionId?", async function(req, res, next) {
-    let regionId = req.params.regionId;
-    console.log(regionId);
-    var regionDao = new Region(req.app.get('db'));    
-    var listRegions = await regionDao.getRegions(regionId);
-    return res.json(listRegions);
+router.get("/:regionId?", [
+    check('regionId')
+        .optional()
+        .isInt().withMessage('Provide a correct value'),
+], async function (req, res) {
+
+    var responseObject = new Response();
+
+    try {
+        var errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            console.log(errors.mapped());
+            responseObject.errors.code = process.env.VALIDATION_ERROR;
+            responseObject.errors.message = errors.mapped();
+            return res.json(responseObject);
+        }
+
+        var regionId = req.params.regionId;
+        var regionDao = new Region(req.app.get('db'));
+        var listRegions = await regionDao.getRegions(regionId);
+        responseObject.data = listRegions;
+        return res.json(responseObject);
+    } catch (err) {
+        console.log(err);
+        responseObject.errors.code = err.code;
+        responseObject.errors.message = err.message;
+        return res.json(responseObject);
+    }
 
 });
 
-router.get("/communes/:regionId", async function(req, res) {
+router.get("/communes/:regionId", [
+    check('regionId')
+        .exists()
+        .isInt().withMessage('Provide a correct value'),
+], async function (req, res) {
 
-    let regionId = req.params.regionId;
-    console.log(regionId);
-    var regionDao = new Region(req.app.get('db'));    
-    var listCommunes = await regionDao.getCommunesByRegion(regionId);
-    return res.json(listCommunes);
+    var responseObject = new Response();
+
+    try {
+        var errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            console.log(errors.mapped());
+            responseObject.errors.code = process.env.VALIDATION_ERROR;
+            responseObject.errors.message = errors.mapped();
+            return res.json(responseObject);
+        }
+
+        let regionId = req.params.regionId;
+        console.log(regionId);
+        var regionDao = new Region(req.app.get('db'));
+        var listCommunes = await regionDao.getCommunesByRegion(regionId);
+        responseObject.data = listCommunes;
+        return res.json(responseObject);
+
+    } catch (err) {
+        console.log(err);
+        responseObject.errors.code = err.code;
+        responseObject.errors.message = err.message;
+        return res.json(responseObject);
+    }
 
 });
 
