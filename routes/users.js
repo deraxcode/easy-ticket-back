f867f0a0042d2b6709f18bcfda206561e8fa2391
Hugo Carreño <hugo.carreno@taisachile.cl>
2018-08-01 15:36:10 -0400

    avance

--- routes/users.js
+++ routes/users.js
@@ -2,15 +2,18 @@ var express = require('express');
 var router = express.Router();
 var app = require("../app");
 var User = require("../model/User");
-
-
+const consts = require("../util/consts")
 
 router.post("/assistant", function(req, res) {
 
     let user = req.body;
 
-    let userDAO = new User(req.app.get('db'));
+    user['user_status_id'] = consts.USER_INACTIVE;
+    user['google'] = false;
 
+    console.log(user);
+
+    let userDAO = new User(req.app.get('db'));
 
     let savedUser = userDAO.save(user);
 
@@ -21,6 +24,7 @@ router.post("/assistant", function(req, res) {
 router.post("/host", function(req, res) {
 
     let user = req.body;
+
 
     console.log(user);
 
---
+++ util/consts.js
@@ -0,0 +1,4 @@
+exports.USER_INACTIVE = 0;
+exports.USER_ACTIVE = 1;
+exports.USER_BLOCKED = 3;
+exports.USER_BANNED = 4;
