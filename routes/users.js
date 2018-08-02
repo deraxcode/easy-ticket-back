7b44db2cae9ce0b671960d31214f260062188227
Hugo Carreño <hugo.carreno@taisachile.cl>
2018-08-01 23:52:08 -0400

    avance

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,38 @@
-var express = require('express');
-var router = express.Router();
-var app = require("../app");
-var User = require("../model/User");
+const express = require('express');
+const router = express.Router();
+const app = require("../app");
+const User = require("../model/User");
 const consts = require("../util/consts")
+const bcrypt = require("bcrypt");
+const { body, check, validationResult } = require('express-validator/check');
 
-router.post("/assistant", function(req, res) {
+router.post("/assistant", [
+    body('first_name').isLength({ min: 1 }),
+    body("last_name").isLength({ min: 1 }),
+    body("email").isEmail(),
+    body("password").isLength({ min: 1 }),
+    body("password_confirm", "invalid password")
+    .custom((value, { req }) => {
+        if (value !== req.body.password) {
+            // trow error if passwords do not match
+            throw new Error("Passwords don't match");
+        } else {
+            return value;
+        }
+    }),
+    body("birth_date").isLength({ min: 1 }),
+], (req, res) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+    }
 
     let user = req.body;
 
     user['user_status_id'] = consts.USER_INACTIVE;
     user['google'] = false;
-
-    console.log(user);
+    user['password'] = bcrypt.hashSync(user.password, 10);
 
     let userDAO = new User(req.app.get('db'));
 
@@ -24,9 +45,6 @@ router.post("/assistant", function(req, res) {
 router.post("/host", function(req, res) {
 
     let user = req.body;
-
-
-    console.log(user);
 
     var userDAO = new User(app.get('db'));
 
--- util/consts.js
+++ util/consts.js
@@ -1,5 +1,5 @@
-exports.USER_INACTIVE = 0;
-exports.USER_ACTIVE = 1;
+exports.USER_INACTIVE = 1;
+exports.USER_ACTIVE = 2;
 exports.USER_BLOCKED = 3;
 exports.USER_BANNED = 4;
 exports.VALIDATION_ERROR_CODE = 001;
